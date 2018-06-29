6f8a9a003784829a870b9419cb9b8b26e8a87fae
alphaecho1229 <eoryal@gmail.com>
2018-06-29 14:11:29 -0400

    Fixed 1st test, trying without library(testthat)

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,8 +1,8 @@
-library(testthat)
+#library(testthat)
 library(Week4Assessment)
 
 #test_check("Week4Assessment")
 
-expect_that(fars_read("badfilename"), shows_message())
+expect_that(fars_read("badfilename"), throws_error())
 
 expect_that(make_filename("2007"), matches("accident_2007.csv.bz2"))
