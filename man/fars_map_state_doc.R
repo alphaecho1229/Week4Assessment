aa398f440b600b4e473532f0f03fd4938f1da815
alphaecho1229 <eoryal@gmail.com>
2018-07-01 17:42:51 -0400

    Moved files around, fixed .R files, generated .Rd files...

---
+++ man/fars_map_state_doc.R
@@ -0,0 +1,56 @@
+#' Generate a map of a state showing locations of accidents throughout a given year.
+#'
+#' This function takes in a numbered code for a state and a year as arguments,
+#' generates a file name, and reads in data from the file into a data frame.
+#'
+#' The function checks to make sure the state number is valid (returns a message "invalid state number" otherwise),
+#' and creates a new dataframe consisting only of accidents from that state.
+#'
+#' This function will also result in an error if an invalid value for year is given.
+#'
+#' If there are no accidents in the state for that year, it returns a message to the user.
+#'
+#' The function then checks for longitude values greater than 900 and
+#' latitude values greater than 90 and turns them into NAs.
+#'
+#' The function then generates the map using longitude and latitude values for the points.
+#'
+#' @param state.num a string or numeric representing the coded value for
+#' a state.
+#'
+#' @param year a string or numeric representing the year for which we want to load accident data.
+#'
+#' @return a map of a state showing locations of the accidents for a given year
+#'
+#' @examples
+#' fars_map_state(05, 2005)
+#'
+#' state <- 05
+#' year <- 2005
+#' fars_map_state(state, year)
+#'
+#' @importFrom dplyr filter
+#' @importFrom maps map
+#' @importFrom graphics points
+#'
+#' @export
+fars_map_state <- function(state.num, year) {
+  filename <- make_filename(year)
+  data <- fars_read(filename)
+  state.num <- as.integer(state.num)
+
+  if(!(state.num %in% unique(data$STATE)))
+    stop("invalid STATE number: ", state.num)
+  data.sub <- dplyr::filter(data, STATE == state.num)
+  if(nrow(data.sub) == 0L) {
+    message("no accidents to plot")
+    return(invisible(NULL))
+  }
+  is.na(data.sub$LONGITUD) <- data.sub$LONGITUD > 900
+  is.na(data.sub$LATITUDE) <- data.sub$LATITUDE > 90
+  with(data.sub, {
+    maps::map("state", ylim = range(LATITUDE, na.rm = TRUE),
+              xlim = range(LONGITUD, na.rm = TRUE))
+    graphics::points(LONGITUD, LATITUDE, pch = 46)
+  })
+}
---
+++ man/fars_summarize_years_doc.R
@@ -0,0 +1,41 @@
+#' Summarize the number of accidents in each month
+#'
+#' This function takes a vector of strings or numerics representing
+#' which years will be summarized. The vector is passed to
+#' fars_read_years to create a list of dataframes.
+#'
+#' The function then calls bind_rows to combine the dataframes into one.
+#' The single dataframe is then passed into group_by and summarize to
+#' obtain the total number of accidents each month.
+#'
+#' Finally, the summarized dataframe is passed to spread to create a dataframe
+#' where the columns are each year, the rows are the months, and the values are the
+#' number of accidents each month for each year.
+#'
+#' If years does not consist of strings or numerics representing the years to summarize,
+#' an error may result.
+#'
+#' @param years a vector of strings or numerics, each in "yyyy" format.
+#'
+#' @return a dataframe consisting of the number of accidents of each month,
+#' with the summarized years as the column names.
+#'
+#' @examples
+#' fars_summarize_years(c(2005, 2010, 2015))
+#'
+#' years <- 2000:2009
+#' df <- fars_summarize_years(years)
+#'
+#' @importFrom dplyr bind_rows
+#' @importFrom dplyr group_by
+#' @importFrom dplyr summarize
+#' @importFrom tidyr spread
+#'
+#' @export
+fars_summarize_years <- function(years) {
+  dat_list <- fars_read_years(years)
+  dplyr::bind_rows(dat_list) %>%
+    dplyr::group_by(year, MONTH) %>%
+    dplyr::summarize(n = n()) %>%
+    tidyr::spread(year, n)
+}
